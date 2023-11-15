d9b689ea555c608e98750e2e2a3823177ac91479
gantuangcoc98 <gantuangco98@yahoo.com.ph>
2023-11-15 23:21:24 +0800

    Edited introduction functionality to a video introduction instead.

--- app/src/main/java/com/example/studyguru/IntroductionActivity.java
+++ app/src/main/java/com/example/studyguru/IntroductionActivity.java
@@ -4,87 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 public class IntroductionActivity extends AppCompatActivity {
 
-    private TextView introductionTextView;
-    private View fullScreenView;
-    private MediaPlayer mediaPlayer;
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introduction);
 
-        introductionTextView = findViewById(R.id.introductionTextView);
-        mediaPlayer = MediaPlayer.create(this, R.raw.introduction);
+        videoView = findViewById(R.id.videoView);
+        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.intro_vid);
+        videoView.setVideoURI(videoUri);
 
-        playAudio();
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                scrollTextSlowly();
-                enableClickToHomePage();
-            }
-        }, 3000);
-    }
-
-    private void scrollTextSlowly() {
-        final Handler handler = new Handler();
-        final int delayMillis = 50;
-
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Scroll the text by adjusting the top padding
-                int paddingTop = introductionTextView.getPaddingTop();
-                paddingTop -= 1; // Adjust this value for the scrolling speed
-                introductionTextView.setPadding(0, paddingTop, 0, 0);
-
-                // Check if the scrolling is complete
-                if (paddingTop + introductionTextView.getHeight() > 0) {
-                    handler.postDelayed(this, delayMillis);
-                }
-            }
-        }, delayMillis);
-    }
-
-    private void enableClickToHomePage() {
-        Toast.makeText(this, "Click anywhere to skip...", Toast.LENGTH_SHORT).show();
-        fullScreenView = findViewById(R.id.fullScreenView);
-        fullScreenView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Launch HomePage when the user clicks anywhere on the screen
-                onDestroy();
-                launchHomePage();
-            }
+        videoView.setOnCompletionListener(mediaPlayer -> {
+            Intent home = new Intent(this, HomePage.class);
+            this.startActivity(home);
         });
-    }
 
-    private void launchHomePage() {
-        Intent homePage = new Intent(this, HomePage.class);
-        this.startActivity(homePage);
-        Toast.makeText(this, "Skipping introduction...", Toast.LENGTH_SHORT).show();
-    };
+        videoView.start();
 
-
-    private void playAudio() {
-        if (mediaPlayer != null) {
-            mediaPlayer.start();
-        }
-    }
-
-    protected void onDestroy() {
-        super.onDestroy();
-        // Release the MediaPlayer resources
-        if (mediaPlayer != null) {
-            mediaPlayer.release();
-        }
+        videoView.setOnClickListener(view -> {
+            videoView.stopPlayback();
+            Intent home = new Intent(this, HomePage.class);
+            this.startActivity(home);
+        });
     }
 }
